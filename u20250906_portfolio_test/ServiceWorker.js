522685718bb2b9bcdad16db22c0ead46c50b3407
mr <a@a>
2025-09-06 13:53:50 +0900

    ゲームの追加のテスト

---
+++ u20250906_portfolio_test/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-u20250830_gun_trpg_card-1.0";
+const contentToCache = [
+    "Build/u20250906_portfolio_test.loader.js",
+    "Build/u20250906_portfolio_test.framework.js",
+    "Build/u20250906_portfolio_test.data",
+    "Build/u20250906_portfolio_test.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
